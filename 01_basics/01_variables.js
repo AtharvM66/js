9fe9ccb9e1258c7aa6e9273eef34d39e2e191720
AtharvM66 <137928350+AtharvM66@users.noreply.github.com>
2024-07-08 18:09:53 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 144553
+//accountId=2
+//console.log(accountId);
+let accountEmail="a@g.com"
+var accountPassword="1234"
+accountCity="Jaipur"
+
+accountEmail="d@g.com"
+accountPassword="4321"
+accountCity="Pune"
+
+//dont use var for declaring variable, use let due to issue in block and func scope
+
+console.table([accountEmail, accountPassword, accountCity])
