263f511b7c3e8db552d1d3c01aa67f5572021739
AtharvM66 <137928350+AtharvM66@users.noreply.github.com>
2024-07-09 09:39:33 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,9 @@
+"use strict";
+let name = "name" //string datatype
+let age = 18 //numerical
+let isLoggedIn = false //boolean
+let x //undefined
+let temperature = null //null value
+//symbol used for unique
+console.log(typeof null) //object 
+console.log(typeof undefined) //undefined
