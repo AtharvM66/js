2ec34b357380712375ef51c294ee9dcd334d986b
AtharvM66 <137928350+AtharvM66@users.noreply.github.com>
2024-07-13 16:43:03 +0000

    conversion

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,11 @@
+//console.log("2">1);
+
+//console.log(null > 0);
+//console.log(null == 0);
+//console.log(null >= 0); // gives true, which should be wrong, avoid these type of conversions
+
+// === is strict check which checks both value and datatype
+// == compares only values
+
+console.log("2" == 2);
+console.log("2" === 2);
